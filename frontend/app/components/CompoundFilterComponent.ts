7e94dadb074ebd440122a38f65d5af33b98eb3ff
Juan Luis Boya García <ntrrgc@gmail.com>
2016-03-15 02:18:26 +0100

    prevent dragged containers from trying to drop inside itself

--- frontend/app/components/CompoundFilterComponent.ts
+++ frontend/app/components/CompoundFilterComponent.ts
@@ -1,5 +1,12 @@
 import CompoundFilter = require("../filters/CompoundFilter");
 
+// http://ejohn.org/blog/comparing-document-position/
+function htmlContains(a: HTMLElement, b: HTMLElement){
+    return a.contains ?
+    a != b && a.contains(b) :
+        !!(a.compareDocumentPosition(b) & 16);
+}
+
 class CompoundFilterComponent {
     filter: CompoundFilter;
     flagText: String;
@@ -13,6 +20,14 @@ class CompoundFilterComponent {
 
     get flagClasses() {
         return ['flag', this.flagClass].join(' ');
+    }
+
+    // tip from http://jsfiddle.net/cfenzo/7chaomnz/
+    dragulaAccept(el: HTMLElement, target: HTMLElement, source: HTMLElement,
+                  sibling: HTMLElement)
+    {
+        // prevent dragged containers from trying to drop inside itself
+        return !htmlContains(el,target);
     }
 }
 
