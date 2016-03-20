d8e7d9b2aa7935e723e22096508169d2a0c0366c
Juan Luis Boya García <ntrrgc@gmail.com>
2016-03-20 15:45:27 +0100

    Fix isInstance()

--- frontend/app/utils/isInstance.ts
+++ frontend/app/utils/isInstance.ts
@@ -1,10 +1,13 @@
 /** Checks types in JavaScript, both class and primitive. */
 function isInstance(object: any, classType: any) {
-    if (typeof object != classType) {
-        return false;
-    }
     if (typeof object == 'object') {
+        // Object instance of class
         return object instanceof classType;
+    } else {
+        // Value instance of primitive type
+        // classType is e.g. Number
+        const typeName = classType.name.toLowerCase();
+        return typeof object == typeName;
     }
 }
 export = isInstance;
