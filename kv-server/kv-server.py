a17cd9b8823e86412adb6d268e24cc04b981b8dd
Juan Luis Boya García <ntrrgc@gmail.com>
2016-05-03 15:11:23 +0200

    Use Tornado

--- kv-server/kv-server.py
+++ kv-server/kv-server.py
@@ -6,10 +6,24 @@ def create_db():
     db.create_all()
 
 
-def run_server(host='0.0.0.0', port=9201, debug=False):
+def run_server(host='localhost', port=9201, debug=False):
     from kv_server.app import app
+
+    # looks unused, but it's actually needed in order to have... well, views.
     import kv_server.views
-    app.run(host, port, debug)
+
+    if debug:
+        # Auto reloads (sort of), but hangs easily :(
+        app.run(host, port, debug)
+    else:
+        # Tornado is needed in order not to get hung on browser connections
+        from tornado.wsgi import WSGIContainer
+        from tornado.httpserver import HTTPServer
+        from tornado.ioloop import IOLoop
+
+        http_server = HTTPServer(WSGIContainer(app))
+        http_server.listen(port, address=host)
+        IOLoop.instance().start()
 
 
 argh.dispatch_commands([
