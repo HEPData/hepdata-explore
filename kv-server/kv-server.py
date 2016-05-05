4d9ebb0c604b22291f7f2f78b250263547676a6a
Juan Luis Boya García <ntrrgc@gmail.com>
2016-05-05 04:26:33 +0200

    Changed Tornado for gevent

--- kv-server/kv-server.py
+++ kv-server/kv-server.py
@@ -46,14 +46,11 @@ def run_server(host='localhost', port=9201, debug=False, db_url=default_db_url,
         # Auto reloads (sort of), but hangs easily :(
         app.run(host, port, debug)
     else:
-        # Tornado is needed in order not to get hung on browser connections
-        from tornado.wsgi import WSGIContainer
-        from tornado.httpserver import HTTPServer
-        from tornado.ioloop import IOLoop
+        # gevent is needed in order not to get hung on browser connections
+        from gevent.wsgi import WSGIServer
 
-        http_server = HTTPServer(WSGIContainer(app))
-        http_server.listen(port, address=host)
-        IOLoop.instance().start()
+        http_server = WSGIServer((host, port), app)
+        http_server.serve_forever()
 
 
 argh.dispatch_commands([
