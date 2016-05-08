26a4b92800f5d72ed84b4f37cf8f80db0099f100
Juan Luis Boya García <ntrrgc@gmail.com>
2016-05-08 14:14:57 +0200

    sed -i

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+set -eu
 chmod -R o+rX .
-exec rsync -Ppr ./ ntrrgc@rufian.eu:/hepdata/hepdata-explore/
+pushd frontend
+gulp
+popd
+rsync -Ppr ./ ntrrgc@rufian.eu:/hepdata/hepdata-explore/
+ssh -T ntrrgc@rufian.eu <<\HERE
+sed -i 's|data-main="build/bootstrap"|data-main="release/bootstrap"|' /hepdata/hepdata-explore/frontend/index.html
+HERE
