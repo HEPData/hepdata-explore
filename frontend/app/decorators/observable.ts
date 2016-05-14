0cbf146a67799c005eee78ea276ddeedae41c3b1
Juan Luis Boya García <ntrrgc@gmail.com>
2016-05-14 20:06:47 +0200

    @observable decorator

---
+++ frontend/app/decorators/observable.ts
@@ -0,0 +1,60 @@
+import {RuntimeError} from "../base/errors";
+export function observable() {
+    return function(... args: any[]): any //target: any, propertyKey: string, descriptor: PropertyDescriptor)
+        // : PropertyDescriptor
+    {
+        let target = args[0];
+        let propertyKey = args[1];
+        let descriptor = args[2];
+
+        // Substitute the property in the prototype with a "proxy" property that
+        // the first time it's read or written instantiates an observable and
+        // sets it as a property in the instance, overriding the aforementioned
+        // proxy prototype.
+        return {
+            enumerable: true,
+            configurable: true,
+            get() {
+                throw new RuntimeError("Observable property read before assigned.");
+            },
+            set(value: any) {
+                // We create the observable property in two steps.
+                // First, we create a POJO property in the instance, initialized
+                // to undefined.
+                Object.defineProperty(this, propertyKey, {
+                    configurable: true,
+                    enumerable: true,
+                    writable: true,
+                    value: undefined,
+                });
+                // Then we use ko.track() to turn it into a Knockout ES5 observable
+                ko.track(this, [propertyKey]);
+                // Finally we return what the new property returns (therefore
+                // we are calling the observable, which is important to keep
+                // ko.computed() observables working.
+                return (this[propertyKey] = value);
+            }
+        }
+    };
+}
+
+/** Example usage */
+class MyClass {
+    @observable()
+    public x = 1;
+
+    constructor() {
+        console.log(this.x); // 1
+    }
+}
+
+function test() {
+    const instance = new MyClass();
+    instance.x = 10;
+    ko.getObservable(instance, 'x').subscribe((x) => {
+        console.log(x);
+    });
+
+    instance.x = 2;
+}
+// test();
