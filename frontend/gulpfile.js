7aba524f1f8637d07d1677eb4f9b50bf6608803d
Juan Luis Boya García <ntrrgc@gmail.com>
2016-06-01 16:03:10 +0200

    Bundle and minify with gulp

--- frontend/gulpfile.js
+++ frontend/gulpfile.js
@@ -2,16 +2,49 @@ var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var ts = require('gulp-typescript');
 var babel = require('gulp-babel');
+var replace = require('gulp-replace');
+var rjs = require('gulp-requirejs-optimize');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var pump = require('pump');
 
 var tsProject = ts.createProject('./tsconfig.json', {
   typescript: require('typescript')
 });
 
-gulp.task('default', function() {
+gulp.task('typescript', function () {
   return gulp.src('app/**/*.ts')
     .pipe(sourcemaps.init())
     .pipe(ts(tsProject))
     .pipe(babel())
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('release'));
+    .pipe(gulp.dest('build-compat'))
 });
+
+gulp.task('bundle', ['typescript'], function () {
+  return gulp.src('build-compat/bootstrap.js')
+    .pipe(rjs({
+      name: 'bootstrap',
+      baseUrl: 'build-compat/',
+      out: 'hepdata-explore.js',
+      optimize: 'none',
+    }))
+    .pipe(gulp.dest('release/'))
+});
+
+gulp.task('minify', ['bundle'], function (cb) {
+  pump([
+    gulp.src('release/hepdata-explore.js'),
+    sourcemaps.init(),
+    uglify(),
+    rename({suffix: '.min'}),
+    sourcemaps.write('.'),
+    gulp.dest('release'),
+  ], cb);
+})
+
+gulp.task('production-replace', function () {
+  //
+});
+
+gulp.task('default', ['typescript', 'bundle', 'minify']);
