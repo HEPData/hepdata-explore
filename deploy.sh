28271aeb454575db7c00d05f3c199b8e14d63516
Juan Luis Boya García <ntrrgc@gmail.com>
2016-06-03 03:25:36 +0200

    Update deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ chmod -R o+rX .
 pushd frontend
 gulp
 popd
-rsync -Ppr --exclude deployment ./ ntrrgc@rufian.eu:/hepdata/hepdata-explore/
-ssh -T ntrrgc@rufian.eu <<\HERE
-sed -i 's|data-main="build/bootstrap"|data-main="release/bootstrap"|' /hepdata/hepdata-explore/frontend/index.html
-HERE
+rsync -Ppr \
+  --exclude deployment \
+  --exclude node_modules \
+  ./ ntrrgc@rufian.eu:/hepdata/hepdata-explore/
